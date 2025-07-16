69698576b9f93901cf5e7fe42ba4c2a3304df5d6
Zhahna <104663648+Zhahna@users.noreply.github.com>
2025-07-16 18:33:07 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Zhahan'
+description 'ND_BankRobbery'
+version '1.0'
+url 'https://github.com/theenack/ND_BankRobbery'
+
+client_scripts {
+	"config.lua",
+	'client.lua'
+}
+
+server_scripts { 
+	"config.lua",
+	"server.lua"
+}
+
+client_script "@Badger-Anticheat/acloader.lua"
